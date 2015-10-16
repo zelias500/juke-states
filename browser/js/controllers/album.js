6cd1000cfca569055a2e66e2c009a28dd1f93967
Zack Elias <zelias500@gmail.com>
2015-10-16 09:55:40 -0400

    Email share button added

--- browser/js/controllers/album.js
+++ browser/js/controllers/album.js
@@ -1,7 +1,8 @@
 app.controller('AlbumCtrl', function ($scope, album, PlayerFactory) {
 
 	$scope.album = album;
-	
+	$scope.email = "mailto:?subject="+$scope.album.name+"&body=Check this out: "+window.location.href
+
 	$scope.isCurrent = function (song) {
 		var current = PlayerFactory.getCurrentSong();
 		return current && current._id == song._id;
